336b46456a3e9adcc5c48dfeccfcc6ce4f54ead9
Justine Retiro <128771486+Justine-Retiro@users.noreply.github.com>
2023-07-06 14:51:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("Programmer: Justine Retiro \nDate Created: 7/6/2023 \nDescription of the Program: Personal information\nFirst Name: Justine\nMiddle Name: Dagatan\nLast Name: Retiro\nCourse: BSIT\nYear: 3rd Year\nAge: 20\nGender: Male\nCivil Status: Single\nNationality: Filipno\nMotto: Philosophy in life: Dont chase butterfly, build a garden and butterflies will eventually come to you.")
+
+num1 = 5
+num2 = 8
+sum = num1 + num2
+print("The sum of number is: ",sum)
